190ed681627b489a360c62c42911b85579df239e
ColleterVi <v_colleter@yahoo.fr>
2019-10-15 16:08:07 +0200

    cross-platform version

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ import requests
 from time import sleep, time
 import argparse
 import platform
+from tqdm import tqdm
+import shutil
 
 system_os = platform.system()
 gecko = {
@@ -13,9 +15,9 @@ gecko = {
     "linux":"./ressources/geckodriver_linux",
     "darwin":"./ressources/geckodriver_mac"
 }
-print(system_os)
+print("Hello fellow ", system_os, ' user.')
 path_gecko = gecko.get(system_os.lower())
-print(path_gecko)
+
 
 parser = argparse.ArgumentParser(description='Recover slides from slide share')
 parser.add_argument("url", type = str, 
@@ -24,53 +26,64 @@ parser.add_argument("url", type = str,
 parser.add_argument("--password", type=str, default = None,
                     help="Password if the file is protected")
 
+parser.add_argument("--output", type=str, default = 'output',
+                    help="name of the output file")
+
+
 
 args = parser.parse_args()
 
-def image_to_pdf(url, password = None):
-    get_image(url, password)
+
+
+def image_to_pdf(url, password, output):
+    with Browser("firefox", executable_path=path_gecko, headless=True) as driver:
+
+        if driver.find_by_id("pvtdoc") is not None:
+            if password is None:
+                exit('A password is requiered, please retry')
+            driver.find_by_id("pvtdoc").fill(password)
+            sleep(1)
+            driver.find_by_value('Submit').first.click()
+
+            sleep(2)  
+
+            images = driver.find_by_tag('img')
+            if images is None:
+                exit("Wrong password, please retry")
+
+            links = []
+
+            for i in images:
+                links.append(i['data-full'])
+            links = list(filter(None, links))
+
+            if not os.path.exists("./tmp"):
+                os.makedirs("./tmp")
+
+            with tqdm(len(links)) as pbar:
+                for i,link in enumerate(links):
+                    pbar.set_description(f"Processing : {i}/{len(links)}")
+                    img_data = requests.get(link).content
+
+                    with open('./tmp/'+str(i)+'.jpg', 'wb') as handler:
+                        handler.write(img_data)
+                        
+                    pbar.update(1/len(links))    
+
     def getint(text):
         return [int(i) for i in re.split("(\d+)",text) if i.isdigit() ]
 
-    os.chdir("./tmp") 
+    os.chdir("./tmp")
     liste_image = [i for i in os.listdir(os.getcwd()) if i.endswith(".jpg")]
     
     liste_image.sort(key=getint)
 
-    with open("lecture1.pdf", "wb") as f:
+    with open("../"+output+".pdf", "wb") as f:
         f.write(img2pdf.convert([i for i in liste_image]))
+    os.chdir('..')
+    if os.path.exists("./tmp"):
+        shutil.rmtree("./tmp")
 
-
-
-def get_image(url, password = None):
-    driver = Browser("firefox", executable_path=path_gecko, headless=False)
-    driver.visit(url)
-
-    if driver.find_by_id("pvtdoc") is not None:
-        if password is None:
-            exit('A password is requiered, please retry')
-        driver.find_by_id("pvtdoc").fill(password)
-        sleep(1)
-        driver.find_by_value('Submit').first.click()
-
-        sleep(2)  
-
-        images = driver.find_by_tag('img')
-        if images is None:
-            exit("Wrong password, please retry")
-
-        links = []
-
-        for i in images:
-            links.append(i['data-full'])
-        links = list(filter(None, links))
-
-        for i,link in enumerate(links):
-            img_data = requests.get(link).content
-            print(img_data, link)
-            os.mkdir("tmp")
-            with open('./tmp/'+str(i)+'.jpg', 'wb') as handler:
-                handler.write(img_data)
 
 if __name__ == "__main__":
-    get_image(args.url, args.password)
+    image_to_pdf(args.url, args.password, args.output)
